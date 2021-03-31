9d15fd7d77c838e2a05702cf76d16a62faae7c64
DESKTOP-42KISGJ\Lenovo <stok.shausiu@gmail.com>
2021-03-31 21:37:33 +0300

    End of lesson 3

--- ConsoleApp3/Lesson3/Program3.cs
+++ ConsoleApp3/Lesson3/Program3.cs
@@ -6,7 +6,33 @@ namespace ConsoleApp3
     {
         static void Main()
         {
-            Console.WriteLine("123");
+            Console.Write("Įveskite skaičių 1: ");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite skaičių 2: ");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Įveskite skaičių 3: ");
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            switch (skaicius1)
+            {
+                case 1: 
+                    Console.WriteLine(skaicius1 + skaicius2 + skaicius3);
+                        break;
+                case 2: 
+                    Console.WriteLine(skaicius1 - skaicius3);
+                    break;
+
+                case 3:
+                    Console.WriteLine(skaicius2 * skaicius3);
+                    break;
+                default:
+                    Console.WriteLine("Klaida");
+                    break;
+
+            }
+
+
+
         }
 
     }
